8f1d52505cc40ce0fe489bbcaeb3c8bb90eddaaf
xieyubai <xieyubai17@gmail.coml>
2016-12-21 16:14:56 +0800

    ues before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new,:create,:edit,:update,:destroy]
+  before_action :find_group_and_check_permission, only: [:edit,:update,:destroy]
   def index
     @groups = Group.all
   end
@@ -13,11 +14,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "你没有权限编辑该文章"
-    end
   end
 
   def create
@@ -32,10 +28,6 @@ class GroupsController < ApplicationController
    end
 
    def update
-     @group = Group.find(params[:id])
-     if current_user != @group.user
-       redirect_to root_path, alert: "你没有权限编辑该文章"
-     end
 
      if @group.update(group_params)
      redirect_to groups_path,notice: "Update Success"
@@ -45,16 +37,21 @@ class GroupsController < ApplicationController
    end
 
    def destroy
-     @group = Group.find(params[:id])
-     if current_user != @group.user
-       redirect_to root_path, alert: "你没有权限编辑该文章"
-     end
+
      @group.destroy
      flash[:alert] = "Group deleted"
      redirect_to groups_path
    end
 
    private
+
+   def find_group_and_check_permission
+     @group = Group.find(params[:id])
+
+     if current_user != @group.user
+       redirect_to root_path,alert: "你没有权限编辑该文章."
+     end
+   end
 
    def group_params
      params.require(:group).permit(:title, :description)
